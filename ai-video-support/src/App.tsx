4cd0756c68dc0188e60ff74ab1344839cd8738c4
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-12 13:14:13 -0700

    Adding left side text area

--- ai-video-support/src/App.tsx
+++ ai-video-support/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/ui/button"
 import { Github } from "lucide-react"
 import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
 
 function App() {
 
@@ -15,6 +16,18 @@ function App() {
           <Button variant="outline"><Github className="w-4 h-4 mr-2" />Github</Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-row-2 gap-4 flex-1">
+            <Textarea className="resize-none p-4 leading-relaxed" placeholder="Include your AI Prompt..." />
+            <Textarea className="resize-none p-4 leading-relaxed" placeholder="Result generated..." readOnly/>
+          </div>
+
+          <p className="text-sm text-muted-foreground">Remember: You can use the variable <code className="text-violet-400">{'{transcription}'}</code> in your prompt to add the content of the transcription for the video selected.</p>
+        </div>
+        <aside className="w-80"></aside>
+      </main>
     </div>
   )
 }
